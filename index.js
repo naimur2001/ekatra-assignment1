9431a4a70694ba6d63d1cee82e3064a14d404e5a
naimur2001 <naimurrahman18thapril@gmail.com>
2023-09-18 12:46:31 +0600

    data post method and savetofile func done

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const express=require("express");
-const cors=require("cors");
+
 const cron=require("node-cron");
 const app=express();
 const fs=require("fs");
 const port =process.env.PORT || 5000 ;
 //middleware
 app.use(express.json());
-app.use(cors());
+
 
 app.get("/",(req,res)=>{
   res.send("Todo List Server is Running.");
@@ -20,7 +20,7 @@ app.listen(port,()=>{
 
 const datas=JSON.parse(fs.readFileSync("Data.json","utf-8"));
 
-app.get("/todos", async (rer,res)=>{
+app.get("/todos",  (rer,res)=>{
   try {
      res.send(datas)
   } catch (error) {
@@ -28,3 +28,22 @@ app.get("/todos", async (rer,res)=>{
     res.send({message:error.message})
   }
 })
+//create todo  list
+app.post("/todos/post", (req,res)=>{
+try {
+  const newData=req.body;
+datas.push(newData);
+saveToFile();
+res.send(newData)
+} catch (error) {
+  console.log(error.message);
+  res.send({message:error.message})
+}
+})
+
+
+//writting in json file function
+const saveToFile=()=>{
+  fs.writeFileSync('Data.json', JSON.stringify(datas, null, 2), 'utf8');
+
+}
